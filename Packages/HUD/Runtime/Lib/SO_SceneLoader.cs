488797f4a4612dbd92f6d554021f0fa0e3065109
kauderk <65237382+kauderk@users.noreply.github.com>
2022-07-07 07:01:47 -0500

    save state

--- Packages/HUD/Runtime/Lib/SO_SceneLoader.cs
+++ Packages/HUD/Runtime/Lib/SO_SceneLoader.cs
@@ -1,19 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "SceneLoader", menuName = "Loaders/SceneLoader")]
 public class SO_SceneLoader : SingletonScriptableObject<SO_SceneLoader>
 {
     public List<SO_SceneData> LoadingData = new List<SO_SceneData>();
+    public SceneUtils SceneUtils = new SceneUtils();
 
+
+    #region Unity State Cycle
     private static GameObject pupetMonoBehaviour;
+
     private void OnEnable()
     {
+        SceneManager.sceneLoaded += UnitySceneManagerLoaded;
         MainManager.OnGameInitialized += OnLoad;
     }
     private void OnDisable()
     {
+        SceneManager.sceneLoaded -= UnitySceneManagerLoaded;
         MainManager.OnGameInitialized -= OnLoad;
         CleanUp();
     }
@@ -25,7 +36,10 @@ public class SO_SceneLoader : SingletonScriptableObject<SO_SceneLoader>
         Destroy(pupetMonoBehaviour);
 #endif
     }
+    #endregion
 
+
+    #region Custom Logic
     private void OnLoad()
     {
         pupetMonoBehaviour = new GameObject("Singleton_SceneLoader");
@@ -36,7 +50,14 @@ public class SO_SceneLoader : SingletonScriptableObject<SO_SceneLoader>
     {
         LoadScene(SO_SceneLoader.Instance.LoadingData.Find(x => x.Name.ToLower().Contains(name.ToLower())));
     }
+    public void UnitySceneManagerLoaded(){
 
+    }
+
+    #endregion
+
+
+    #region Function Wrappers
     public static void LoadScene(SwipeMenu.Menu MenuRef)
     {
         pupetMonoBehaviour.GetComponent<LoaderUtils>().LoadScene(MenuRef);
@@ -61,8 +82,32 @@ public class SO_SceneLoader : SingletonScriptableObject<SO_SceneLoader>
     {
         pupetMonoBehaviour.GetComponent<LoaderUtils>().LoadScene(data.Scene);
     }
+    #endregion
+
 }
-public class LaadAssets
+
+[System.Serializable]
+public class SceneUtils
 {
-    public SO_SceneData Loaders;
+    public GameObject FaderCanvas;
+    public Image fader;
+    public Toggle GlobalVolumeToogle;
+    public static SceneUtils Instance;
+    event Action OnFaderLoaded;
+    public bool isForcedToLoad = false;
+
+    public SceneUtils()
+    {
+        fader = FaderCanvas.GetComponentInChildren<Image>();
+
+        // Instance = this;
+        // OnFaderLoaded += () =>
+        // {
+        //     FaderCanvas.SetActive(false);
+        //     fader.gameObject.SetActive(false);
+        //     GlobalVolumeToogle.gameObject.SetActive(false);
+        // };
+    }
+    public void ToogleFader(bool b) => fader.gameObject.SetActive(b);
+
 }
