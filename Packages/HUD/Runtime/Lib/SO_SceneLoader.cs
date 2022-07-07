78c023748773c330daa7437c4fe99f4a7649a133
kauderk <65237382+kauderk@users.noreply.github.com>
2022-07-07 07:07:56 -0500

    save state

--- Packages/HUD/Runtime/Lib/SO_SceneLoader.cs
+++ Packages/HUD/Runtime/Lib/SO_SceneLoader.cs
@@ -45,13 +45,16 @@ public class SO_SceneLoader : SingletonScriptableObject<SO_SceneLoader>
         pupetMonoBehaviour = new GameObject("Singleton_SceneLoader");
         pupetMonoBehaviour.AddComponent<LoaderUtils>();
         DontDestroyOnLoad(pupetMonoBehaviour);
+
+
     }
     public static void RequestSceneLoad(string name)
     {
         LoadScene(SO_SceneLoader.Instance.LoadingData.Find(x => x.Name.ToLower().Contains(name.ToLower())));
     }
-    public void UnitySceneManagerLoaded(){
-
+    public void UnitySceneManagerLoaded(Scene scene, LoadSceneMode mode)
+    {
+        //AudioListener.pause = GlobalVolumeToogle.isOn;
     }
 
     #endregion
@@ -99,15 +102,53 @@ public class SceneUtils
     public SceneUtils()
     {
         fader = FaderCanvas.GetComponentInChildren<Image>();
-
-        // Instance = this;
-        // OnFaderLoaded += () =>
-        // {
-        //     FaderCanvas.SetActive(false);
-        //     fader.gameObject.SetActive(false);
-        //     GlobalVolumeToogle.gameObject.SetActive(false);
-        // };
     }
     public void ToogleFader(bool b) => fader.gameObject.SetActive(b);
 
+    public void FadeIn()
+    {
+        var buffer = 20;
+        fader.rectTransform.sizeDelta = new Vector2(Screen.width + buffer, Screen.height + buffer);
+        ToogleFader(false);
+    }
+    public async UniTask LoadScene<T>(T sceneID, float duration = 1, float waitTime = 2)
+    {
+        // YIKES!
+        if (UnitySceneUtils.DoesSceneExist(sceneID))
+        {
+            await FadeScene(sceneID, duration, waitTime);
+            return;
+        }
+        Debug.Log("Scene ID must be string or int");
+    }
+    async UniTask FadeScene<T>(T sceneID, float duration, float waitTime)
+    {
+        ToogleFader(true);
+
+        await LerpFader(0, 1);
+
+        await UniTask.Delay(TimeSpan.FromSeconds(waitTime), ignoreTimeScale: false);
+
+        var ao = typeof(T) == typeof(int)
+            ? SceneManager.LoadSceneAsync((int)(object)sceneID)
+            : SceneManager.LoadSceneAsync((string)(object)sceneID); // https://stackoverflow.com/questions/4092393/value-of-type-t-cannot-be-converted-to#:~:text=string%20newT2%20%3D%20(string)(object)t%3B
+
+        while (!ao.isDone || isForcedToLoad)
+            await UniTask.Yield();
+
+        await LerpFader(1, 0);
+
+        ToogleFader(false);
+
+        OnFaderLoaded?.Invoke();
+
+        async UniTask LerpFader(int from, int to)
+        {
+            for (float t = 0; t < 1; t += Time.deltaTime / duration)
+            {
+                fader.color = new Color(0, 0, 0, Mathf.Lerp(from, to, t));
+                await UniTask.Yield();
+            }
+        }
+    }
 }
