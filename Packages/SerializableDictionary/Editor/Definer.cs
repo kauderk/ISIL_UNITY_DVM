44c034490ba6f2278e63c063365360bb84f27ce8
kauderk <65237382+kauderk@users.noreply.github.com>
2022-07-10 14:08:40 -0500

    solved errors for SerializableDictionary
    using EditorOnly within it's asmdf

--- Packages/SerializableDictionary/Editor/Definer.cs
+++ Packages/SerializableDictionary/Editor/Definer.cs
@@ -1,85 +1,27 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using UnityEditor;
 
-namespace RotaryHeart.Lib
+namespace RotaryHeart.Lib.SerializableDictionary
 {
-    public class Definer
+    [InitializeOnLoad]
+    public class SerializableDictionaryDefiner : Definer
     {
-        /// <summary>
-        /// Applies the defines to the script symbols
-        /// </summary>
-        /// <param name="defines">List of defines to add</param>
-        public static void ApplyDefines(List<string> defines)
+        static SerializableDictionaryDefiner()
         {
-            if (defines == null || defines.Count == 0)
+            List<string> defines = new List<string>(1)
             {
-                return;
-            }
-
-            string availableDefines = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
-            List<string> definesSplit = new List<string>(availableDefines.Split(';'));
-
-            foreach (string define in defines)
-            {
-                if (!definesSplit.Contains(define))
-                {
-                    definesSplit.Add(define);
-                }
-            }
-
-            _ApplyDefine(string.Join(";", definesSplit.ToArray()));
-        }
-
-        /// <summary>
-        /// Removes the defines from the script symbols
-        /// </summary>
-        /// <param name="defines">List of defines to remove</param>
-        public static void RemoveDefines(List<string> defines)
-        {
-            if (defines == null || defines.Count == 0)
-                return;
-
-            string availableDefines = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
-            List<string> definesSplit = new List<string>(availableDefines.Split(';'));
-
-            foreach (string define in defines)
-            {
-                definesSplit.Remove(define);
-            }
-
-            _ApplyDefine(string.Join(";", definesSplit.ToArray()));
-        }
-
-        /// <summary>
-        /// Returns true if a define is already defined
-        /// </summary>
-        /// <param name="define">Define to check</param>
-        public static bool ContainsDefine(string define)
-        {
-            if (string.IsNullOrEmpty(define))
-            {
-                return false;
-            }
+                "RH_SerializedDictionary"
+            };
             
-            string availableDefines = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
-            List<string> definesSplit = new List<string>(availableDefines.Split(';'));
-
-            return definesSplit.Contains(define);
-        }
-
-        /// <summary>
-        /// Actual logic that applies the defines symbols
-        /// </summary>
-        /// <param name="define">List of defines to save, this includes the already defined ones</param>
-        static void _ApplyDefine(string define)
-        {
-            if (string.IsNullOrEmpty(define))
+            if (string.IsNullOrEmpty(AssetDatabase.GUIDToAssetPath("ccdbbd5c7cb114949a686ae4fe06d208")))
             {
-                return;
+                RemoveDefines(new List<string>(1)
+                {
+                    "RH_SerializedDictionaryPro"
+                });
             }
 
-            PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup, define);
+            ApplyDefines(defines);
         }
     }
-    
 }
