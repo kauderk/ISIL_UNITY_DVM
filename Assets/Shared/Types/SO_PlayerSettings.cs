af442aed8c329bac5dc52f63366a987c6bcd3de3
kauderk <65237382+kauderk@users.noreply.github.com>
2022-07-12 09:15:16 -0500

    added basic shared SO_PlayerSettings

---
+++ Assets/Shared/Types/SO_PlayerSettings.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerSettings", menuName = "Game Data/PlayerSettings", order = 1)]
+public class SO_PlayerSettings : ScriptableObject
+{
+    public float rotationSpeed = 200f;
+    public float movementSpeed = 5.0f;
+    public GameObject dustTrailBackPrefab;
+    public GameObject dustTrailPrefab;
+
+    public Animator animator { get; private set; }
+    public ParticleSystem dustTrail { get; private set; }
+    public ParticleSystem dustTrailBack { get; private set; }
+
+    public void Init(GameObject self)
+    {
+        animator = self.GetComponent<Animator>();
+        dustTrail = Instantiate(dustTrailPrefab).GetComponent<ParticleSystem>();
+        dustTrailBack = Instantiate(dustTrailBackPrefab).GetComponent<ParticleSystem>();
+    }
+}
