8bbde4c058a4bef3978d20d519831cd748d87483
kauderk <65237382+kauderk@users.noreply.github.com>
2022-07-12 17:42:51 -0500

    modified Behaviour CameraController.cs

--- Packages/Behavior/Runtime/Scripts/CameraController.cs
+++ Packages/Behavior/Runtime/Scripts/CameraController.cs
@@ -3,20 +3,24 @@ using Photon.Pun;
 
 public class CameraController : MonoBehaviourPunCallbacks
 {
-    private Vector3 distance = new Vector3(0, 15, -20);
-    private Transform myCamera = null;
+    [field: SerializeField]
+    public Transform myCamera { get; private set; }
+
+    Vector3 distance = new Vector3(0, 15, -20);
 
     private void Awake()
     {
-        if(photonView.IsMine)
+        if (!photonView.IsMine)
         {
-            myCamera = GameObject.FindGameObjectWithTag("Camera").transform; 
-            myCamera.transform.position = this.transform.position + distance;
+            Debug.Log("CameraController: Awake: Not mine... disabling");
+            enabled = false;
         }
 
+        myCamera = GameObject.FindGameObjectWithTag("Camera").transform; //FIXME: 
+        myCamera.transform.position = transform.position + distance;
     }
     void Update()
     {
-        if (photonView.IsMine) myCamera.transform.position = this.transform.position + distance;
+        myCamera.transform.position = transform.position + distance;
     }
 }
