e492e28584586e6a1728bb26dd8e2063a7c4e775
kauderk <65237382+kauderk@users.noreply.github.com>
2022-07-13 18:35:01 -0500

    added utils/generic under UnityEngine namespace

---
+++ Assets/Shared/Types/Util/Generics.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Linq;
+
+namespace UnityEngine
+{
+    public static class Generics
+    {
+        public static T GetComponentInChildren<T>(this GameObject gameObject) where T : Component
+        {
+            return gameObject.GetComponentsInChildren<T>().FirstOrDefault();
+        }
+        public static void NotifySiblings<I>(this Transform transform, Action<I> OnInterface) where I : class
+        {
+            transform.parent.GetComponentsInChildren<I>().ToList().ForEach(OnInterface);
+        }
+        // NotifyAllParents
+        public static void NotifyParent<I>(this Transform transform, Action<I> OnInterface) where I : class
+        {
+            transform.GetComponentsInParent<I>().ToList().ForEach(OnInterface);
+        }
+    }
+}
