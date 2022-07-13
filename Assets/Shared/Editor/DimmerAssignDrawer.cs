0715702001ced92c9e2d39033af3f8450bc4a600
kauderk <65237382+kauderk@users.noreply.github.com>
2022-07-13 14:42:45 -0500

    added DimmerAssign and NonExpandable Editor Attributes

---
+++ Assets/Shared/Editor/DimmerAssignDrawer.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEditor;
+
+public class DimmerAssign : PropertyAttribute
+{
+    public Color color;
+
+    public DimmerAssign(Color _color) => color = _color;
+    public DimmerAssign() => color = new Color(1, 1, 1, 0.5f); // semi transparent color
+}
+
+// https://www.anton.website/colored-inspector-fields/
+[CustomPropertyDrawer(typeof(DimmerAssign))]
+public class DimmerAssignDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        var field = attribute as DimmerAssign;
+
+        if (property.objectReferenceValue != null)
+        {
+            // semi transparent color
+            GUI.color = field.color; //Set the color of the GUI
+            EditorGUI.PropertyField(position, property, label); //Draw the GUI
+            GUI.color = Color.white; //Reset the color of the GUI to white
+        }
+        else
+        {
+            EditorGUI.PropertyField(position, property, label);
+        }
+    }
+}
--- Assets/Shared/Editor/ScriptableObjectDrawer.cs
+++ Assets/Shared/Editor/ScriptableObjectDrawer.cs
@@ -1,10 +1,27 @@
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
+
+/// <summary>
+/// Use this property on a ScriptableObject type to allow the editors drawing the field to draw an expandable
+/// area that allows for changing the values on the object without having to change editor.
+/// </summary>
+public class NonExpandableAttribute : PropertyAttribute
+{
+    public NonExpandableAttribute() { }
+}
+[CustomPropertyDrawer(typeof(NonExpandableAttribute), true)]
+public class NonExpandableAttributeDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        // override the default
+        EditorGUI.PropertyField(position, property, label, false);
+    }
+}
 // https://forum.unity.com/threads/editor-tool-better-scriptableobject-inspector-editing.484393/
 [CustomPropertyDrawer(typeof(ScriptableObject), true)]
-public class ScriptableObjectDrawerF : PropertyDrawer
+public class ScriptableObjectDrawer : PropertyDrawer
 {
     // Cached scriptable object editor
     private Editor editor = null;
