94b1e145f2c5719506b17698b04f83684755f8f8
kauderk <65237382+kauderk@users.noreply.github.com>
2022-07-14 08:42:26 -0500

    added cameraFollow prefab to CreatePlayer on SO_DependencyManager.cs

--- Assets/Shared/Photon/SO_DependencyManager.cs
+++ Assets/Shared/Photon/SO_DependencyManager.cs
@@ -7,9 +7,22 @@ using Photon.Pun;
 public class SO_DependencyManager : SingletonScriptableObject<SO_DependencyManager>
 {
     public GameObject playerPrefab;
+    public GameObject cameraFollowPrefab;
     public void CreatePlayer()
     {
-        PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(0f, 12f, 0f), Quaternion.identity);
+        var cam = PhotonNetwork.Instantiate(cameraFollowPrefab.name, new Vector3(0f, 20f, -20f), Quaternion.identity);
+        cam.SetActive(false);
+        var camController = cam.GetComponent<RS_CameraController>();
+
+        var player = PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(0f, 12f, 0f), Quaternion.identity);
+        player.SetActive(false);
+
+        camController.AssignTarget(player.transform);
+
+        player.transform.NotifyChildren<ICameraEvents>(I => I.OnCameraAnimatorChange(cam.GetComponentInChildren<Animator>()));
+
+        player.SetActive(true);
+        cam.SetActive(true);
     }
     public void Instantiate(GameObject prefab, Vector3 position, Quaternion rotation)
     {
