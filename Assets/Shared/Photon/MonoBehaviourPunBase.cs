468cf1ec79b5bd7d75044e5a0a766ed7a2070acf
kauderk <65237382+kauderk@users.noreply.github.com>
2022-07-16 07:33:33 -0500

    added isMine() to MonoBehaviourPunBase
    MyAwake & MyUpdate

--- Assets/Shared/Photon/MonoBehaviourPunBase.cs
+++ Assets/Shared/Photon/MonoBehaviourPunBase.cs
@@ -1,16 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
-using Photon.Pun;
-using UnityEngine;
-
 namespace Photon.Pun
 {
     public class MonoBehaviourPunBase : MonoBehaviourPun
     {
-        // Update is called once per frame
+        bool isMine() => photonView.IsMine || SO_DependencyManager.Instance.CreatePlayerOffline;
+
+        // void OnEnable()
+        // {
+        //     if (isMine())
+        //         MyOnEnable();
+        // }
+        // protected virtual void MyOnEnable() { }
+
+        void Awake()
+        {
+            if (isMine())
+                MyAwake();
+        }
+        protected virtual void MyAwake() { }
+
+        // void Start() {
+        //     if (!isMine())
+        //         MyStart();
+        // }
+        // protected virtual void MyStart(){}
+
         void Update()
         {
-            if (photonView.IsMine)
+            if (isMine())
                 MyUpdate();
         }
         protected virtual void MyUpdate() { }
