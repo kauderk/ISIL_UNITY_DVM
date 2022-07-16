a03b2342965dd228d0563117f3b0b12309281fdf
kauderk <65237382+kauderk@users.noreply.github.com>
2022-07-16 07:32:31 -0500

    added OnGameInitialized registration cycle

--- Assets/Shared/Types/Manager/MainManager.cs
+++ Assets/Shared/Types/Manager/MainManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "MainManager", menuName = "Singleton/MainManager")]
@@ -12,5 +13,29 @@ public class MainManager : SingletonScriptableObject<MainManager>
         {
             OnGameInitialized?.Invoke();
         }
+    }
+    public static readonly List<Action> OnGameInitializedActions = new List<Action>();
+    public static void RegisterOnGameInitialized(Action action)
+    {
+        OnGameInitializedActions.Add(action);
+        OnGameInitialized += action;
+    }
+    private void OnDisable()
+    {
+        CleanOnGameInitializedActions();
+        GlobalEvents.ClanIDToEventMap();
+    }
+
+    private static void CleanOnGameInitializedActions()
+    {
+        Array.ForEach(OnGameInitializedActions.ToArray(), (action) =>
+        {
+            if (action == null)
+                return;
+
+            foreach (var cb in action.GetInvocationList())
+                action -= (Action)cb;
+            OnGameInitializedActions.Remove(action);
+        });
     }
 }
