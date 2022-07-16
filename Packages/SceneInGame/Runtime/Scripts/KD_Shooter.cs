caa89d3d77c7986db336ad0a2c55567cdc1320fb
kauderk <65237382+kauderk@users.noreply.github.com>
2022-07-16 11:48:17 -0500

    introducing sound states to KD_Shooter.cs

--- Packages/SceneInGame/Runtime/Scripts/KD_Shooter.cs
+++ Packages/SceneInGame/Runtime/Scripts/KD_Shooter.cs
@@ -11,27 +11,42 @@ namespace Weapon
 
         IWeaponShooter IShooter;
         float clockRate;
+        SO_WeaponMagazine mag;
 
         protected override void MyAwake()
         {
+            mag = Settings.Magazine;
             IShooter = WeaponSettings.shooter; //SO_WeaponShooter
             IShooter.Init(WeaponSettings.Magazine, WeaponSettings.bulletSettings, EditorSettings);
         }
 
         protected override void MyUpdate()
         {
-            if (InputStoppedShooting())
+            var input = InputShoot();
+
+            if (mag.Busy)
+                return;
+
+            if (input.up)
             {
                 Play(S => S.Release);
                 transform.NotifySiblings<IFireEvent>(I => I.OnStopFire());
             }
-            if (!InputIsShooting())
-                return;
-
             clockRate += Time.deltaTime; // update the clock
 
-            if (!IShooter.CanFire(clockRate))
+            if (!input.down || !input.hold)
                 return;
+
+            if (input.down && !mag.hasAmmo())
+            {
+                Play(S => S.Empty);
+                //return;
+            }
+            if (!IShooter.Elapsed(clockRate) || !mag.hasAmmo())
+            {
+                //Play(S => S.Dry);
+                return;
+            }
 
             if (IShooter.Type == WeaponType.automatic)
                 clockRate = 0f; // reset timer
@@ -45,5 +60,12 @@ namespace Weapon
         private void Play(Func<SO_WeaponSFX, AudioClip> cb) => WeaponSettings.SFX.Play(cb);
         bool InputIsShooting() => Input.GetMouseButton(0) || Input.GetMouseButtonDown(0);
         bool InputStoppedShooting() => Input.GetButtonUp("Fire1");
+        (bool down, bool up, bool hold) InputShoot()
+        {
+            var down = Input.GetButtonDown("Fire1");
+            var up = Input.GetButtonUp("Fire1");
+            var hold = Input.GetMouseButton(0);
+            return (down, up, hold);
+        }
     }
 }
