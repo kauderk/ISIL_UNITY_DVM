668bda413bd985db183f17197a1805c58da212a3
kauderk <65237382+kauderk@users.noreply.github.com>
2022-07-16 11:47:04 -0500

    add comments to SO_WeaponSFX.cs

--- Assets/Shared/Types/SO_WeaponSFX.cs
+++ Assets/Shared/Types/SO_WeaponSFX.cs
@@ -6,12 +6,19 @@ namespace Weapon
     [CreateAssetMenu(fileName = "WeaponSFX", menuName = "Game Data/WeaponSFX")]
     public class SO_WeaponSFX : ScriptableObject
     {
+        [Header("Shoot")]
         public AudioClip Fire;
         public AudioClip Release;
-        public AudioClip Reload;
         public AudioClip Empty;
         public AudioClip Burst;
-        public AudioClip Exhaust;
+        public AudioClip Dry;
+
+        [Header("Reload")]
+        public AudioClip SingleReload;
+        public AudioClip FullyReloaded;
+        public AudioClip BusyReloading;
+
+        [Header("Reaction")]
         public AudioClip PickUp;
         public AudioClip Drop;
         public AudioClip Equip;
@@ -23,6 +30,7 @@ namespace Weapon
                 return;
             AudioSource.PlayClipAtPoint(clip, Vector3.zero);
         }
+        // Shooter and Reloader have same function, how you abstract that?
         public void Play(Func<SO_WeaponSFX, AudioClip> cb) => this.Play(cb?.Invoke(this));
     }
 }
