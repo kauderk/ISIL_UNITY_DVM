7d76a656c9b64d8a25ac11721d68b76f83a9fb97
kauderk <65237382+kauderk@users.noreply.github.com>
2022-07-17 03:01:48 -0500

    removed OnCollisionEnter from KD_Reloader.cs

--- Packages/SceneInGame/Runtime/Scripts/KD_Reloader.cs
+++ Packages/SceneInGame/Runtime/Scripts/KD_Reloader.cs
@@ -1,12 +1,10 @@
-using System;
 using UnityEngine;
 
 namespace Weapon
 {
-
     public class KD_Reloader : KD_MonoWeapon
     {
-        [SerializeField, HideInInspector, Tooltip("Inherited from the WeaponSettings.")]
+        [SerializeField, HideInInspector, Tooltip("Inherited from WeaponSettings")]
         public SO_WeaponReloader Settings;
 
         protected override void MyUpdate()
@@ -27,22 +25,6 @@ namespace Weapon
             if (delta > WeaponSettings.SFX.FullyReloaded.length)
             {
                 Resolve(BusyMagazine: false);
-            }
-        }
-
-        void OnCollisionEnter(Collision collision)
-        {
-            if (collision.gameObject.TryGetComponent<IMagazine>(out var magazine))
-            {
-                collision.gameObject.SetActive(false);
-                //Debug.Break();
-                //SO_RedirectSignalManager.Signal.PickUp(gameObject);
-                SO_RedirectSignalManager.UseUpwardSignal<KD_ISignalListener>(gameObject);
-                SO_RedirectSignalManager.UseLateralSignal<KD_ISignalListener>(gameObject);
-                //var settings = magazine.PickUp();
-                //Iweapon.FillMagazine();
-                // TODO: Fire a Gloabl Event to update the UI 
-                //txtBulletCount.color = settings.color;
             }
         }
     }
