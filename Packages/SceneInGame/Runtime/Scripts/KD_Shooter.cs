15f96d42a838b3582ad5e5bfb66ef59d5bcf5b12
kauderk <65237382+kauderk@users.noreply.github.com>
2022-07-17 03:02:18 -0500

    added EventBus OnCollisionWithMagazine to KD_Shooter.cs

--- Packages/SceneInGame/Runtime/Scripts/KD_Shooter.cs
+++ Packages/SceneInGame/Runtime/Scripts/KD_Shooter.cs
@@ -1,25 +1,36 @@
 using System;
+using EventBusSystem;
 using UnityEngine;
 
 namespace Weapon
 {
-    public class KD_Shooter : KD_MonoWeapon
+    public class KD_Shooter : KD_MonoWeapon, ICollisionSubscriber
     {
-        [SerializeField, HideInInspector, Tooltip("Inherited from the WeaponSettings.")]
+        [SerializeField, HideInInspector, Tooltip("Inherited from WeaponSettings")]
         public SO_WeaponShooter Settings;
         public SOC_WeaponShooter EditorSettings;
 
         IWeaponShooter IShooter;
+
+        private void OnEnable() => EventBus.Subscribe(this);
+        private void OnDisable() => EventBus.Unsubscribe(this);
+
+        public void OnCollisionWithMagazine(Collision collision)
+        {
+            var magazine = collision.gameObject.GetComponent<IMagazine>();
+            var Ammo = Store.SO_Artillery.Instance.Ammo[magazine.settings.Type];
+            IShooter.Init(Ammo: Ammo, skin: magazine.skinSettings);
+        }
 
         protected override void MyAwake()
         {
             base.MyAwake();
             delta = Settings.Burst + 1; // able to shoot on start up
             IShooter = WeaponSettings.Shooter; //SO_WeaponShooter
-            IShooter.Init(WeaponSettings.Magazine, WeaponSettings.Ammo, EditorSettings);
+            IShooter.Init(WeaponSettings.Ammo, EditorSettings, WeaponSettings.Skin);
         }
 
-        protected override void MyUpdate()
+        protected override void MyUpdate() // Use a State Machine FIXME:
         {
             if (Magazine.Busy)
                 return;
